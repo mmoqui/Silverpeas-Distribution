6246cbb844bfef456f827d6291395acc094e83b0
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2015-02-03 19:38:49 +0100

    Fix some bugs and takes into account new features provided by the plugin SilverpeasSetup: the deployment and the undeployment of artifacts

--- configuration/silverpeas/jcrSettings.groovy
+++ configuration/silverpeas/jcrSettings.groovy
@@ -12,16 +12,21 @@ import java.nio.file.Path
 
 println 'Configure the JCR repository'
 
-String jcrHomePath = Service.getPath(settings.JCR_HOME)
-String jcrConfigurationPath = "${jcrHomePath}/repository.xml"
-Path jcrConfigurationTemplate = Service.getPath("${settings.CONFIGURATION_HOME}/silverpeas/repository.jcr")
+Path jcrHomePath = Service.getPath(settings.JCR_HOME)
+Path jcrConfigurationPath = Service.getPath("${settings.JCR_HOME}/repository.xml")
+Path jcrConfigurationTemplatePath = Service.getPath("${settings.CONFIGURATION_HOME}/silverpeas/repository.jcr")
 
 /* creates the JCR home directory if it doesn't already exist and copies the JCR configuration file
 * into this directory */
 Service.createDirectory(jcrHomePath, [readable: true, writable: true, executable: true])
-Path jcrConfiguration = Service.getPath(jcrConfigurationPath)
-if (!Files.exists(jcrConfiguration)) {
-  Files.copy(jcrConfigurationTemplate, jcrConfiguration)
+if (!Files.exists(jcrConfigurationPath)) {
+  Files.copy(jcrConfigurationTemplatePath, jcrConfigurationPath)
+}
+
+/* case of an old workspace named jackrabbit */
+Path oldWorkspace = Service.getPath("${settings.JCR_HOME}/workspaces/jackrabbit")
+if (Files.exists(oldWorkspace)) {
+  Files.move(oldWorkspace, Service.getPath("${settings.JCR_HOME}/workspaces/silverpeas"))
 }
 
 /* Update the JCR configuration file from the Silverpeas configuration properties */
@@ -29,9 +34,9 @@ SAXParserFactory factory = javax.xml.parsers.SAXParserFactory.newInstance()
 factory.validating = false
 factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd",
     false)
-def jcrRepositoryConf = new XmlSlurper(factory.newSAXParser()).parse(new File(jcrConfigurationPath))
+def jcrRepositoryConf = new XmlSlurper(factory.newSAXParser()).parse(jcrConfigurationPath.toFile())
 jcrRepositoryConf.Workspace.PersistenceManager.@class = settings.JACKRABBIT_PERSISTENCE_MANAGER
 jcrRepositoryConf.Workspace.PersistenceManager.param.find { it.@name == 'schema'}.@value = settings.DB_SCHEMA
 jcrRepositoryConf.Versioning.PersistenceManager.@class = settings.JACKRABBIT_PERSISTENCE_MANAGER
-XmlUtil.serialize(jcrRepositoryConf, new FileWriter(jcrConfigurationPath))
+XmlUtil.serialize(jcrRepositoryConf, new FileWriter(jcrConfigurationPath.toFile()))
 
