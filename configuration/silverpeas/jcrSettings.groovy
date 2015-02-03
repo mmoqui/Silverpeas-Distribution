e3b5c80b4c43ed04c924200e7484d9e99cf43825
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2015-02-03 22:32:05 +0100

    Take into account of the old JCR repositories coming from Silverpeas 5.14.x

--- configuration/silverpeas/jcrSettings.groovy
+++ configuration/silverpeas/jcrSettings.groovy
@@ -16,6 +16,11 @@ Path jcrHomePath = Service.getPath(settings.JCR_HOME)
 Path jcrConfigurationPath = Service.getPath("${settings.JCR_HOME}/repository.xml")
 Path jcrConfigurationTemplatePath = Service.getPath("${settings.CONFIGURATION_HOME}/silverpeas/repository.jcr")
 
+SAXParserFactory factory = javax.xml.parsers.SAXParserFactory.newInstance()
+factory.validating = false
+factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd",
+    false)
+
 /* creates the JCR home directory if it doesn't already exist and copies the JCR configuration file
 * into this directory */
 Service.createDirectory(jcrHomePath, [readable: true, writable: true, executable: true])
@@ -24,16 +29,21 @@ if (!Files.exists(jcrConfigurationPath)) {
 }
 
 /* case of an old workspace named jackrabbit */
-Path oldWorkspace = Service.getPath("${settings.JCR_HOME}/workspaces/jackrabbit")
-if (Files.exists(oldWorkspace)) {
-  Files.move(oldWorkspace, Service.getPath("${settings.JCR_HOME}/workspaces/silverpeas"))
+Path oldWorkspacePath = Service.getPath("${settings.JCR_HOME}/workspaces/jackrabbit")
+if (Files.exists(oldWorkspacePath)) {
+  Path newWorkspacePath = Service.getPath("${settings.JCR_HOME}/workspaces/silverpeas")
+  Files.move(oldWorkspacePath, newWorkspacePath)
+  def jcrWorkspaceConf = new XmlSlurper(factory.newSAXParser())
+      .parse(new File('workspace.xml', newWorkspacePath.toFile()))
+  jcrWorkspaceConf.@name ='silverpeas'
+  jcrWorkspaceConf.PersistenceManager.param.find { it.@name == 'url'}.@value = 'java:/datasources/DocumentStore'
+  XmlUtil.serialize(jcrWorkspaceConf,
+      new FileWriter(new File('workspace.xml', newWorkspacePath.toFile())))
 }
 
+
 /* Update the JCR configuration file from the Silverpeas configuration properties */
-SAXParserFactory factory = javax.xml.parsers.SAXParserFactory.newInstance()
-factory.validating = false
-factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd",
-    false)
+
 def jcrRepositoryConf = new XmlSlurper(factory.newSAXParser()).parse(jcrConfigurationPath.toFile())
 jcrRepositoryConf.Workspace.PersistenceManager.@class = settings.JACKRABBIT_PERSISTENCE_MANAGER
 jcrRepositoryConf.Workspace.PersistenceManager.param.find { it.@name == 'schema'}.@value = settings.DB_SCHEMA
