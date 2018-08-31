63f020457e48d8f094ef59ede2f3cd6c24db0364
Miguel Moquillon <miguel.moquillon@gmail.com>
2018-08-31 14:02:01 +0200

    Fix the webserver-parameters.cli script. Add log instructions in some scipts

--- src/main/dist/configuration/jboss/silverpeas-weblib.groovy
+++ src/main/dist/configuration/jboss/silverpeas-weblib.groovy
@@ -1,6 +1,8 @@
 import java.nio.file.Files
 import java.nio.file.Path
 
+log.info 'Inform Wildfly to deploy explicitly the weblib war'
+
 Path doDeployTagPath = "${settings.SILVERPEAS_DATA_WEB}/weblib.war.dodeploy".asPath()
 if (!Files.exists(doDeployTagPath)) {
   Files.createFile(doDeployTagPath)
--- src/main/dist/configuration/jboss/silverpeas-website.groovy
+++ src/main/dist/configuration/jboss/silverpeas-website.groovy
@@ -1,7 +1,7 @@
-package jboss
-
 import java.nio.file.Files
 import java.nio.file.Path
+
+log.info 'Inform Wildfly to deploy explicitly the website war'
 
 Path doDeployTagPath = "${settings.SILVERPEAS_DATA_WEB}/website.war.dodeploy".asPath()
 if (!Files.exists(doDeployTagPath)) {
