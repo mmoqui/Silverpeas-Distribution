fa6d68d4a2d0311fb401c56b79d0fb25aff3c5cf
Miguel Moquillon <miguel.moquillon@gmail.com>
2018-09-21 13:10:18 +0200

    Add a new script to update very old and deprecated link to FileServer in WYSIWYG contents

---
+++ src/main/dist/configuration/silverpeas/01-updateAttachmentsWithFileServer.groovy
@@ -0,0 +1,22 @@
+import java.nio.file.Path
+
+/**
+ * This script scans all the existing WYSIWYGs to replace the links targeting FileServer and written
+ * in the old and deprecated format by a link respecting the new format.
+ * @author mmoquillon
+ */
+
+log.info 'Replace old FileServer links in existing WYSIWYG contents'
+
+Path workspacePath = "${settings.SILVERPEAS_DATA_HOME}/workspaces".asPath()
+workspacePath.toFile().eachDirRecurse { dir ->
+  dir.eachFileMatch(~/.*wysiwyg.*.txt/) { wysiwyg ->
+    String content = wysiwyg.text
+    String replaced = content.replaceAll(/FileServer\/([a-zA-Z0-9.-_]+)\?ComponentId=([a-zA-Z0-9]+)&(amp;)?attachmentId=(\d+)/,
+        'attached_file/componentId/$2/attachmentId/$4/lang/fr/name/$1')
+    if (replaced != content) {
+      log.info " -> replace old link(s) in ${wysiwyg.path}"
+      wysiwyg.write(replaced)
+    }
+  }
+}
