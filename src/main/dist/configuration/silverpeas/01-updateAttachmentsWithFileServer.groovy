f9c97bc509f9f058c9a5e6788ea44b2123a89566
Miguel Moquillon <miguel.moquillon@gmail.com>
2018-10-03 17:23:14 +0200

    Apply the script 01-updateAttachmentsWithFileServer.groovy only if the workspaces directory exist (it doesn't yet exist in a fresh installation)

--- src/main/dist/configuration/silverpeas/01-updateAttachmentsWithFileServer.groovy
+++ src/main/dist/configuration/silverpeas/01-updateAttachmentsWithFileServer.groovy
@@ -1,3 +1,4 @@
+import java.nio.file.Files
 import java.nio.file.Path
 
 /**
@@ -9,14 +10,16 @@ import java.nio.file.Path
 log.info 'Replace old FileServer links in existing WYSIWYG contents'
 
 Path workspacePath = "${settings.SILVERPEAS_DATA_HOME}/workspaces".asPath()
-workspacePath.toFile().eachDirRecurse { dir ->
-  dir.eachFileMatch(~/.*wysiwyg.*.txt/) { wysiwyg ->
-    String content = wysiwyg.text
-    String replaced = content.replaceAll(/FileServer\/([a-zA-Z0-9.-_]+)\?ComponentId=([a-zA-Z0-9]+)&(amp;)?attachmentId=(\d+)/,
-        'attached_file/componentId/$2/attachmentId/$4/lang/fr/name/$1')
-    if (replaced != content) {
-      log.info " -> replace old link(s) in ${wysiwyg.path}"
-      wysiwyg.write(replaced)
+if (Files.exists(workspacePath) && Files.isDirectory(workspacePath)) {
+  workspacePath.toFile().eachDirRecurse { dir ->
+    dir.eachFileMatch(~/.*wysiwyg.*.txt/) { wysiwyg ->
+      String content = wysiwyg.text
+      String replaced = content.replaceAll(/FileServer\/([a-zA-Z0-9.-_]+)\?ComponentId=([a-zA-Z0-9]+)&(amp;)?attachmentId=(\d+)/,
+          'attached_file/componentId/$2/attachmentId/$4/lang/fr/name/$1')
+      if (replaced != content) {
+        log.info " -> replace old link(s) in ${wysiwyg.path}"
+        wysiwyg.write(replaced)
+      }
     }
   }
 }
